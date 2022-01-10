64425378163c5baa3a5726d8518b6e7e175ab0a9
Shubhi Sharma <shubhi.sharma@newsignature.com>
2022-01-10 12:41:16 +0530

    Master

--- AllAboutCSharp/Program.cs
+++ AllAboutCSharp/Program.cs
@@ -6,7 +6,24 @@ namespace AllAboutCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool Equal = Calculator.AreTheyEqual<string>("ab", "ab");
+
+            if(Equal)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+            }
+        }
+    }
+
+    class Calculator
+    {
+        static public bool AreTheyEqual<T>(T Value1, T Value2)
+        {
+            return Value1.Equals(Value2);
         }
     }
 }
