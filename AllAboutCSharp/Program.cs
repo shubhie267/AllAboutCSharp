37a4259f115a9cea70e3345e040242abf5ecc3eb
Shubhi Sharma <shubhi.sharma@newsignature.com>
2022-01-10 12:31:30 +0530

    Add project files.

---
+++ AllAboutCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AllAboutCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
